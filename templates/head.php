2e8dfbd99d0a35bf432da342dc9ec0211f9d3e58
Eric Brelsford <ebrelsford@gmail.com>
2015-04-14 12:42:06 -0400

    Favico

--- templates/head.php
+++ templates/head.php
@@ -5,5 +5,9 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
+
+    <link rel="icon" href="<?php bloginfo('template_directory'); ?>/dist/images/favicon.png" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/dist/images/favicon.png" />
+
     <?php wp_head(); ?>
   </head>
